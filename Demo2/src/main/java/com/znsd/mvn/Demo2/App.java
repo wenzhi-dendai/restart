f71ca871f8577b7b116fe77a9a40a763f8862fa9
未知等待 <1093789339@qq.com>
2019-08-14 15:18:31 +0800

    增加

---
+++ Demo2/src/main/java/com/znsd/mvn/Demo2/App.java
@@ -0,0 +1,13 @@
+package com.znsd.mvn.Demo2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Demo2/src/main/java/com/znsd/mvn/Demo2/Demo2Test.java
@@ -0,0 +1,10 @@
+package com.znsd.mvn.Demo2;
+
+import com.znsd.mvn.demo1.Test;
+
+public class Demo2Test {
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.test();
+	}
+}
